2a52636492d35e27da0ec82ecc04aefd5454c8b5
Richard Hull <rm_hull@yahoo.co.uk>
2012-11-25 22:24:33 +0000

    What-if and combination solvers

--- app/models/sudoku/Solver.scala
+++ app/models/sudoku/Solver.scala
@@ -8,11 +8,15 @@ trait Solver {
   def ??? = throw new Error("NYI")
 }
 
+/**
+ * Iteratively reduces down the named grid until either a solution is
+ * found (and the solution is returned) or successive reductions produce
+ * no improvements.
+ */
 object SimpleSolver extends Solver {
 
-  private def knownDigits(groups: List[IndexedSeq[Choices]]): Choices =
+  private def knownDigits(cells: IndexedSeq[Choices]): Choices =
     (for {
-      cells <- groups
       cell <- cells
       if (cell.size == 1)
     } yield cell.head).toSet
@@ -25,7 +29,7 @@ object SimpleSolver extends Solver {
    */
   private def singleCandidate(grid: Grid, x: Int, y: Int): Choices = {
     val a = grid.cell(x, y)
-    val b = knownDigits(List(grid.row(y), grid.column(x), grid.box(x,y)))
+    val b = knownDigits(grid.row(y) ++  grid.column(x) ++  grid.box(x, y))
     a diff b
   }
 
@@ -41,14 +45,13 @@ object SimpleSolver extends Solver {
    * position is the only candidate, but is hidden among other candidates.
    */
   private def hiddenSingle(grid: Grid, x: Int, y: Int): Choices = {
-    def chk(f: => IndexedSeq[Choices], digit: Int): Boolean = //true
-      freq(f)(digit) == 1
+    def chk(f: => IndexedSeq[Choices], digit: Int): Boolean = freq(f)(digit) == 1
 
     val uniq = (for {
                   poss <- grid.cell(x, y)
                   if (chk(grid.row(y), poss) ||
                       chk(grid.column(x), poss) ||
-                      chk(grid.box(x,y), poss))
+                      chk(grid.box(x, y), poss))
                 } yield poss)
 
     if (uniq.size == 1) uniq
@@ -69,10 +72,10 @@ object SimpleSolver extends Solver {
       }
     }
 
-    grid.updated(cells.toIndexedSeq)
+    grid.updated(cells)
   }
 
-  def compose[T](f: T => T, g: T => T): T => T = { x: T => f(g(x)) }
+  private def compose[T](f: T => T, g: T => T) = { x: T => f(g(x)) }
 
   def solve(grid: Grid): Grid = {
     val f = compose(reducer(singleCandidate)(_:Grid), reducer(hiddenSingle)(_:Grid))
@@ -81,5 +84,45 @@ object SimpleSolver extends Solver {
       else iter(curr, f(curr))
     }
     iter(Grid.empty, grid)
+  }
+}
+
+/**
+ * Performs a depth-first search of all the possible grids, returned as a
+ * lazy sequence. It is up to the consumer of the sequence to terminate
+ * iterating over the sequence on receipt of a completed solution. Note
+ * that this is different to the simple solver implementation.
+ */
+object WhatIfSolver extends Solver {
+
+  /**
+   * Returns a map comprised of the first cell which has a choice of
+   * possible values, along with the offset in the grid.
+   */
+  private def firstChoice(grid: Grid): Option[(Choices, Int)] =
+    grid.cells.zipWithIndex.find { case(choice, idx) => choice.size > 1 }
+
+  def nextGenerations(grid: Grid): Set[Grid] = firstChoice(grid) match {
+    case None => Set.empty
+    case Some((choices, idx)) => for {
+      poss <- choices
+    } yield SimpleSolver.solve(grid.updated(idx, Set(poss)))
+  }
+
+  def from(initial: Stream[Grid]): Stream[Grid] = initial match {
+    case Stream() => Stream.empty
+    case x #:: xs => x #:: from(nextGenerations(x).toStream append xs)
+  }
+
+  def solve(grid: Grid): Grid =
+    from(Stream(grid)).find(x => x.isSolved).getOrElse(grid)
+}
+
+object CombinationSolver extends Solver {
+
+  def solve(grid: Grid): Grid = {
+    val firstPass = SimpleSolver.solve(grid)
+    if (grid.isSolved) firstPass
+    else WhatIfSolver.solve(firstPass)
   }
 }
