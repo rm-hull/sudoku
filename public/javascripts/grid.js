23e6fd88b87f111669e3555a66417ab80b1143a3
Richard Hull <rm_hull@yahoo.co.uk>
2012-11-30 01:00:14 +0000

    Ajax json call out to server

--- public/javascripts/grid.js
+++ public/javascripts/grid.js
@@ -1,16 +1,36 @@
 $(document).ready(function() {
 
+    $('#spinner').hide();
+    
     var currentCell;
+
+    var successHandler = function(data, textSuccess, jqXHR) {
+        //alert("All done");
+        console.log(data);
+    }
+
+    var errorHandler = function() {
+        alert("Error");
+    }
 
     $('#solve-button').click(function(e) {
         e.preventDefault();
-        alert("Coming soon");
+        $('#spinner').show();
+        $.getJSON(
+            "solve", 
+            {
+                name: "Dave", 
+                grid: $('.box td').text() 
+            }
+        ).done(successHandler)
+         .fail(errorHandler)
+         .always(function() { $('#spinner').hide(); });
     });
 
     $('#clear-button').click(function(e) {
         e.preventDefault();
         $('.box td').each(function(idx, elem) {
-            $(elem).text("");
+            $(elem).text(" ");
         });
     });
 
@@ -19,16 +39,17 @@ $(document).ready(function() {
         currentCell = $(this);
         $('.box td').css("background-color", "white");
         currentCell.css("background-color", "#FAF0E6");
-        $('#input-pad').fadeIn();
+        $('#input-pad').show();
     });
 
     $('#input-pad td,#input-pad th').click(function(e) {
         e.preventDefault();
         var digit = $(this).text();
         if (digit != "Cancel") {
-            currentCell.text(digit == "Clear" ? "" : digit);
+            currentCell.text(digit == "Clear" ? " " : digit);
         }
         currentCell.css("background-color", "white");
         $('#input-pad').delay(250).fadeOut();
     });
+
 });
